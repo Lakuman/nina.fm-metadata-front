e56b4683bc4278427912c75d607f6b0af62db492
Adrien Roussel <Roussel-adrien@outlook.fr>
2017-12-01 14:49:16 +0100

    Correct responsive

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   	<div id="app">
-    	<div class="sidebar col-md-2 col-sm-2">
+    	<div class="sidebar col-md-2 col-sm-2 col-xs-2 col-2">
         	<router-view name = "sidebar"/></router-view>
     	</div>
-    	<div class="main col-md-10 col-sm-10">
+    	<div class="main col-md-10 col-sm-10 col-xs-10 col-10">
       		<router-view/></router-view>
     	</div>
   	</div>
